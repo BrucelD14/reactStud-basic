34a0b73100c4c5402f5313b5ac7c583481a0cee8
BrucelD14 <samuderaduta144@gmail.com>
2022-10-28 11:53:10 +0700

    add app.css, add event handling

--- public/app.js
+++ public/app.js
@@ -1,22 +1,9 @@
-/**
-        const root = document.querySelector('#root');
-        console.log(root);
-        
-        const element = document.createElement('h1');
-        console.log(element);
-        element.textContent = 'Hello from JS';
-        element.className = 'heading-1';
-        
-        root.appendChild(element);
-        **/
-
-const root = document.querySelector('#root');
-console.log("hello dari");
-function tick() {
-  const element = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Jam Sekarang"), /*#__PURE__*/React.createElement("h2", null, new Date().toLocaleTimeString()));
-  ReactDOM.render(element, root);
+const root = document.querySelector("#root");
+function saatKlik() {
+  alert(1);
 }
-tick();
-setInterval(function () {
-  tick();
-}, 1000);
+;
+const element = /*#__PURE__*/React.createElement("button", {
+  onClick: saatKlik
+}, "Click Me");
+ReactDOM.render(element, root);
--- src/app.js
+++ src/app.js
@@ -1,32 +1,15 @@
-/**
-        const root = document.querySelector('#root');
-        console.log(root);
-        
-        const element = document.createElement('h1');
-        console.log(element);
-        element.textContent = 'Hello from JS';
-        element.className = 'heading-1';
-        
-        root.appendChild(element);
-        **/
+const root = document.querySelector("#root");
 
-        const root = document.querySelector('#root');
+function saatKlik() {
+    alert(1);
+  };
 
-        console.log("hello dari");
+const element = (
+  <button
+    onClick={saatKlik}
+  >
+    Click Me
+  </button>
+);
 
-        function tick() {
-            const element = (
-            <div>
-                <h1>Jam Sekarang</h1>
-                <h2>{new Date().toLocaleTimeString()}</h2>
-            </div>
-        )
-
-        ReactDOM.render(element, root);
-        }
-        
-        tick();
-
-        setInterval(function() {
-            tick();
-        }, 1000);
+ReactDOM.render(element, root);
