213cda65416aa852f063cd7cbd10ef9288493670
BrucelD14 <samuderaduta144@gmail.com>
2022-10-29 13:11:06 +0700

    learn component lifecycle, add useEffect

--- public/app.js
+++ public/app.js
@@ -1,16 +1,22 @@
 const root = document.querySelector("#root");
 function App() {
-  const state = React.useState(0);
-  const count = state[0];
-  const updateCount = state[1];
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("button", {
+  const [diklik, setDiklik] = React.useState(false);
+  const [count, setCount] = React.useState(0);
+  React.useEffect(function () {
+    console.log(document.getElementById("judul"));
+  }, [diklik]);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+    id: "judul"
+  }, "Hello ini judul"), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
-      updateCount(count - 1);
+      setDiklik(true);
     }
-  }, "-"), /*#__PURE__*/React.createElement("span", null, count), /*#__PURE__*/React.createElement("button", {
+  }, "Klik aku dong"), /*#__PURE__*/React.createElement("button", {
     onClick: function () {
-      updateCount(count + 1);
+      setCount(count + 1);
     }
-  }, "+"));
+  }, "Tambah"), "Nilai saat ini: ", count);
 }
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
+setTimeout(function () {
+  ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
+}, 1000);
--- src/app.js
+++ src/app.js
@@ -1,30 +1,38 @@
 const root = document.querySelector("#root");
 
 function App() {
-  const state = React.useState(0);
+  const [diklik, setDiklik] = React.useState(false);
+  const [count, setCount] = React.useState(0);
 
-  const count = state[0];
-  const updateCount = state[1];
+  React.useEffect(
+    function () {
+      console.log(document.getElementById("judul"));
+    },
+    [diklik]
+  );
 
   return (
     <>
+      <h1 id="judul">Hello ini judul</h1>
       <button
         onClick={function () {
-          updateCount(count - 1);
+          setDiklik(true);
         }}
       >
-        -
+        Klik aku dong
       </button>
-      <span>{count}</span>
       <button
         onClick={function () {
-          updateCount(count + 1);
+          setCount(count + 1);
         }}
       >
-        +
+        Tambah
       </button>
+      Nilai saat ini: {count}
     </>
   );
 }
 
-ReactDOM.render(<App />, root);
+setTimeout(function () {
+  ReactDOM.render(<App />, root);
+}, 1000);
