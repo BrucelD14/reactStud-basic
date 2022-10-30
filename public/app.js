24a16ee9995738acbd903d26f4e270bf07baaf71
BrucelD14 <samuderaduta144@gmail.com>
2022-10-30 22:11:18 +0700

    add react list

--- public/app.js
+++ public/app.js
@@ -1,14 +1,10 @@
 const root = document.querySelector("#root");
 function App() {
-  const [login, setLogin] = React.useState(false);
-  const judulRef = React.useRef(null);
-  React.useEffect(function () {
-    setTimeout(function () {
-      judulRef.current.textContent = 'Aplikasi';
-    }, 1000);
-  }, []);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
-    ref: judulRef
-  }, "Application"));
+  const fruits = ["Orange", "Avocado", "Banana", "Peach", "Grape"];
+  return /*#__PURE__*/React.createElement("ul", null, fruits.map(function (fruit) {
+    return /*#__PURE__*/React.createElement("li", {
+      key: fruit
+    }, fruit);
+  }));
 }
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,20 +1,14 @@
 const root = document.querySelector("#root");
 
 function App() {
-  const [login, setLogin] = React.useState(false);
-  const judulRef = React.useRef(null);
-
-  React.useEffect(function () {
-    setTimeout(function() {
-      judulRef.current.textContent = 'Aplikasi';
-
-    }, 1000)
-  }, []);
+  const fruits = ["Orange", "Avocado", "Banana", "Peach", "Grape"];
 
   return (
-    <>
-      <h1 ref={judulRef}>Application</h1>
-    </>
+    <ul>
+      {fruits.map(function (fruit) {
+        return <li key={fruit}>{fruit}</li>;
+      })}
+    </ul>
   );
 }
 
